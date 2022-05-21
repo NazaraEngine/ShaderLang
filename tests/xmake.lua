bc7f9cdeb045c898ce0c48a164f65e40fd37757f
Jérôme Leclercq <lynix680@gmail.com>
2022-05-21 20:16:14 +0200

    Disable Unity Build on Catch main

--- tests/xmake.lua
+++ tests/xmake.lua
@@ -19,8 +19,9 @@ if has_config("tests") then
         set_kind("binary")
         set_group("Tests")
         add_headerfiles("src/**.hpp")
+        add_files("src/main.cpp", {unity_ignored = true})
         add_files("src/**.cpp")
 
-		add_deps("nzsl")
+	add_deps("nzsl")
         add_packages("catch2", "glslang", "spirv-tools")
 end
