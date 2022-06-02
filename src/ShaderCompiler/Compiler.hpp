9a757d604bcd0ac7a196f23689f5c703d915372c
SirLynix <lynix680@gmail.com>
2022-06-02 19:46:41 +0200

    Refactor nzslc and add support for benchmarking

---
+++ src/ShaderCompiler/Compiler.hpp
@@ -0,0 +1,91 @@
+// Copyright (C) 2022 Jérôme "Lynix" Leclercq (lynix680@gmail.com)
+// This file is part of the "Nazara Shading Language" project
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NZSLC_COMPILER_HPP
+#define NZSLC_COMPILER_HPP
+
+#include <NZSL/Config.hpp>
+#include <NZSL/ShaderLangErrors.hpp>
+#include <NZSL/Ast/Module.hpp>
+#include <cxxopts.hpp>
+#include <filesystem>
+#include <type_traits>
+#include <vector>
+
+namespace nzslc
+{
+	class Compiler
+	{
+		public:
+			static constexpr std::uint32_t MajorVersion = 0;
+			static constexpr std::uint32_t MinorVersion = 1;
+			static constexpr std::uint32_t PatchVersion = 0;
+
+			Compiler(cxxopts::ParseResult& options);
+			Compiler(const Compiler&) = delete;
+			Compiler(Compiler&&) = delete;
+			~Compiler() = default;
+
+			void HandleParameters();
+
+			void PrintError(const nzsl::Error& error) const;
+			void PrintSource() const;
+
+			void Process();
+
+			Compiler& operator=(const Compiler&) = delete;
+			Compiler& operator=(Compiler&&) = delete;
+
+			static cxxopts::Options BuildOptions();
+
+			enum class LogFormat
+			{
+				Classic,
+				VisualStudio
+			};
+
+		private:
+			void Compile();
+			void CompileToGLSL(std::filesystem::path outputPath, const nzsl::Ast::Module& module);
+			void CompileToNZSL(std::filesystem::path outputPath, const nzsl::Ast::Module& module);
+			void CompileToNZSLB(std::filesystem::path outputPath, nzsl::Ast::ModulePtr& module);
+			void CompileToSPV(std::filesystem::path outputPath, const nzsl::Ast::Module& module);
+			void PrintTime();
+			void OutputFile(std::filesystem::path filePath, const void* data, std::size_t size);
+			void ReadInput();
+			void Sanitize();
+			template<typename F, typename... Args> auto Step(std::enable_if_t<!std::is_member_function_pointer_v<F>, std::string_view> stepName, F&& func, Args&&... args) -> decltype(std::invoke(func, std::forward<Args>(args)...));
+			template<typename F, typename... Args> auto Step(std::enable_if_t<std::is_member_function_pointer_v<F>, std::string_view> stepName, F&& func, Args&&... args) -> decltype(std::invoke(func, this, std::forward<Args>(args)...));
+			template<typename F> auto StepInternal(std::string_view stepName, F&& func) -> decltype(func());
+			nzsl::Ast::ModulePtr Unserialize(const std::uint8_t* data, std::size_t size);
+
+			static nzsl::Ast::ModulePtr Parse(std::string_view sourceContent, const std::string& filePath);
+			static std::vector<std::uint8_t> ReadFileContent(const std::filesystem::path& filePath);
+			static std::string ReadSourceFileContent(const std::filesystem::path& filePath);
+			static void WriteFileContent(const std::filesystem::path& filePath, const void* data, std::size_t size);
+
+			struct StepTime
+			{
+				std::size_t iterationCount = 0;
+				std::size_t childrenCount = 0;
+				std::string name;
+				long long time = 0;
+			};
+
+			std::filesystem::path m_inputFilePath;
+			std::filesystem::path m_outputPath;
+			std::vector<StepTime> m_steps;
+			LogFormat m_logFormat;
+			nzsl::Ast::ModulePtr m_shaderModule;
+			cxxopts::ParseResult& m_options;
+			bool m_measureTime;
+			unsigned int m_iterationCount;
+	};
+}
+
+#include <ShaderCompiler/Compiler.inl>
+
+#endif // NZSLC_COMPILER_HPP
