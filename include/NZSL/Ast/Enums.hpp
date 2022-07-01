19e305cc01697a7885644340fe4f39ee9047a0f8
SirLynix <lynix680@gmail.com>
2022-07-01 08:52:52 +0200

    Ast: Make values explicit to stabilize serialization

--- include/NZSL/Ast/Enums.hpp
+++ include/NZSL/Ast/Enums.hpp
@@ -14,45 +14,45 @@ namespace nzsl::Ast
 {
 	enum class AssignType
 	{
-		Simple,             //< a = b
-		CompoundAdd,        //< a += b
-		CompoundDivide,     //< a /= b
-		CompoundMultiply,   //< a *= b
-		CompoundLogicalAnd, //< a &&= b
-		CompoundLogicalOr,  //< a ||= b
-		CompoundSubtract,   //< a -= b
+		Simple             = 0, //< a = b
+		CompoundAdd        = 1, //< a += b
+		CompoundDivide     = 2, //< a /= b
+		CompoundMultiply   = 3, //< a *= b
+		CompoundLogicalAnd = 4, //< a &&= b
+		CompoundLogicalOr  = 5, //< a ||= b
+		CompoundSubtract   = 6, //< a -= b
 	};
 
 	enum class AttributeType
 	{
-		Binding,            //< Binding (external var only) - has argument index
-		Builtin,            //< Builtin (struct member only) - has argument type
-		Cond,               //< Conditional compilation option - has argument expr
-		DepthWrite,         //< Depth write mode (function only) - has argument type
-		EarlyFragmentTests, //< Entry point (function only) - has argument on/off
-		Entry,              //< Entry point (function only) - has argument type
-		Export,             //< Exported (external block, function and struct only)
-		Layout,             //< Struct layout (struct only) - has argument style
-		Location,           //< Location (struct member only) - has argument index
-		LangVersion,        //< NZSL version - has argument version string
-		Set,                //< Binding set (external var only) - has argument index
-		Unroll,             //< Unroll (for/for each only) - has argument mode
+		Binding            =  0, //< Binding (external var only) - has argument index
+		Builtin            =  1, //< Builtin (struct member only) - has argument type
+		Cond               =  2, //< Conditional compilation option - has argument expr
+		DepthWrite         =  3, //< Depth write mode (function only) - has argument type
+		EarlyFragmentTests =  4, //< Entry point (function only) - has argument on/off
+		Entry              =  5, //< Entry point (function only) - has argument type
+		Export             =  6, //< Exported (external block, function and struct only)
+		Layout             =  7, //< Struct layout (struct only) - has argument style
+		Location           =  8, //< Location (struct member only) - has argument index
+		LangVersion        =  9, //< NZSL version - has argument version string
+		Set                = 10, //< Binding set (external var only) - has argument index
+		Unroll             = 11, //< Unroll (for/for each only) - has argument mode
 	};
 
 	enum class BinaryType
 	{
-		Add,        //< +
-		CompEq,     //< ==
-		CompGe,     //< >=
-		CompGt,     //< >
-		CompLe,     //< <=
-		CompLt,     //< <
-		CompNe,     //< <=
-		Divide,     //< /
-		Multiply,   //< *
-		LogicalAnd, //< &&
-		LogicalOr,  //< ||
-		Subtract,   //< -
+		Add        = 0,  //< +
+		CompEq     = 1,  //< ==
+		CompGe     = 2,  //< >=
+		CompGt     = 3,  //< >
+		CompLe     = 4,  //< <=
+		CompLt     = 5,  //< <
+		CompNe     = 6,  //< <=
+		Divide     = 7,  //< /
+		LogicalAnd = 9,  //< &&
+		LogicalOr  = 10, //< ||
+		Multiply   = 8,  //< *
+		Subtract   = 11, //< -
 	};
 
 	enum class BuiltinEntry
@@ -69,45 +69,45 @@ namespace nzsl::Ast
 
 	enum class DepthWriteMode
 	{
-		Greater,
-		Less,
-		Replace,
-		Unchanged,
+		Greater   = 0,
+		Less      = 1,
+		Replace   = 2,
+		Unchanged = 3,
 	};
 
 	enum class ExpressionCategory
 	{
-		LValue,
-		RValue
+		LValue = 0,
+		RValue = 1
 	};
 
 	enum class IntrinsicType
 	{
-		ArraySize = 10,
-		CrossProduct = 0,
-		DotProduct = 1,
-		Exp = 7,
-		Inverse = 11,
-		Length = 3,
-		Max = 4,
-		Min = 5,
-		Normalize = 9,
-		Pow = 6,
-		Reflect = 8,
+		ArraySize     = 10,
+		CrossProduct  = 0,
+		DotProduct    = 1,
+		Exp           = 7,
+		Inverse       = 11,
+		Length        = 3,
+		Max           = 4,
+		Min           = 5,
+		Normalize     = 9,
+		Pow           = 6,
+		Reflect       = 8,
 		SampleTexture = 2,
-		Transpose = 12
+		Transpose     = 12
 	};
 
 	enum class LoopUnroll
 	{
-		Always,
-		Hint,
-		Never
+		Always = 0,
+		Hint   = 1,
+		Never  = 2
 	};
 
 	enum class MemoryLayout
 	{
-		Std140
+		Std140 = 0
 	};
 
 	enum class NodeType
@@ -121,18 +121,18 @@ namespace nzsl::Ast
 
 	enum class PrimitiveType
 	{
-		Boolean, //< bool
-		Float32, //< f32
-		Int32,   //< i32
-		UInt32,  //< u32
-		String   //< str
+		Boolean = 0, //< bool
+		Float32 = 1, //< f32
+		Int32   = 2, //< i32
+		UInt32  = 3, //< u32
+		String  = 4  //< str
 	};
 
 	enum class UnaryType
 	{
-		LogicalNot, //< !v
-		Minus,      //< -v
-		Plus,       //< +v
+		LogicalNot = 0, //< !v
+		Minus      = 1, //< -v
+		Plus       = 2, //< +v
 	};
 }
 
