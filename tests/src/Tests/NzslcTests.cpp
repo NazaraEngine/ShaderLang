523524daabc404dc8888d0ba2204e1f6f95b4f36
Lynix <lynix680@gmail.com>
2022-07-22 19:38:48 +0200

    Improve cli tests

--- tests/src/Tests/NzslcTests.cpp
+++ tests/src/Tests/NzslcTests.cpp
@@ -1,11 +1,58 @@
+#include <Nazara/Utils/Algorithm.hpp>
 #include <Nazara/Utils/CallOnExit.hpp>
 #include <NZSL/Config.hpp>
 #include <catch2/catch.hpp>
 #include <fmt/format.h>
 #include <process.hpp>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <string_view>
+
+void CheckHeaderMatch(const std::filesystem::path& originalFilepath)
+{
+	std::ifstream originalFile(originalFilepath, std::ios::in | std::ios::binary);
+	REQUIRE(originalFile);
+
+	originalFile.seekg(0, std::ios::end);
+
+	std::streamsize length = originalFile.tellg();
+	REQUIRE(length > 0);
+	if (length == 0)
+		return; //< ignore empty files
+
+	originalFile.seekg(0, std::ios::beg);
+
+	std::vector<char> originalContent(Nz::SafeCast<std::size_t>(length));
+	REQUIRE(originalFile.read(&originalContent[0], length));
+
+	std::filesystem::path headerFilepath = originalFilepath;
+	headerFilepath.concat(".h");
+
+	std::ifstream headerFile(headerFilepath, std::ios::in);
+	REQUIRE(headerFile);
+
+	std::vector<char> content;
+
+	for (std::size_t i = 0; i < originalContent.size(); ++i)
+	{
+		std::uint8_t referenceValue = static_cast<std::uint8_t>(originalContent[i]);
+
+		unsigned int value;
+		headerFile >> value;
+
+		if (value != referenceValue)
+			REQUIRE(value == referenceValue);
+
+		char sep;
+		headerFile >> sep;
+
+		if (sep != ',')
+			REQUIRE(sep == ',');
+	}
+
+	CHECK(headerFile.eof());
+}
 
 void ExecuteCommand(const std::string& command, const std::string& pattern = {})
 {
@@ -25,7 +72,7 @@ void ExecuteCommand(const std::string& command, const std::string& pattern = {})
 	int exitCode = compiler.get_exit_status();
 	if (exitCode != 0)
 	{
-		INFO("Command-line: " << command << "\nstderr: " << errOutput);
+		INFO("Command-line: " << command << "\nstdout: " << output << "\nstderr: " << errOutput);
 		REQUIRE(exitCode == 0);
 	}
 
@@ -61,6 +108,7 @@ TEST_CASE("Standalone compiler", "[NZSLC]")
 			std::filesystem::remove("../resources/Shader.nzslb");
 			std::filesystem::remove("../resources/modules/Color.nzslb");
 			std::filesystem::remove("../resources/modules/Data/OutputStruct.nzslb");
+			std::filesystem::remove_all("UnitTests");
 		};
 
 		Cleanup();
@@ -74,6 +122,18 @@ TEST_CASE("Standalone compiler", "[NZSLC]")
 		ExecuteCommand("./nzslc ../resources/modules/Data/DataStruct.nzslb"); //< validation
 
 		// Try to generate a full shader based on partial compilation result
-		ExecuteCommand("./nzslc --compile=glsl,spv-txt --output=@stdout -m ../resources/modules/Color.nzslb  -m ../resources/modules/Data/OutputStruct.nzslb -m ../resources/modules/Data/DataStruct.nzslb ../resources/Shader.nzslb");
+		ExecuteCommand("./nzslc --compile=glsl,glsl-header,nzsl,nzsl-header,nzslb,nzslb-header,spv,spv-header,spv-txt -o UnitTests -m ../resources/modules/Color.nzslb  -m ../resources/modules/Data/OutputStruct.nzslb -m ../resources/modules/Data/DataStruct.nzslb ../resources/Shader.nzslb");
+		
+		// Validate generated files
+		ExecuteCommand("./nzslc UnitTests/Shader.nzsl");
+		ExecuteCommand("./nzslc UnitTests/Shader.nzslb");
+		ExecuteCommand("glslangValidator -S frag UnitTests/Shader.glsl");
+		ExecuteCommand("spirv-val UnitTests/Shader.spv");
+
+		// Check that header version matches original files
+		CheckHeaderMatch("UnitTests/Shader.glsl");
+		CheckHeaderMatch("UnitTests/Shader.nzsl");
+		CheckHeaderMatch("UnitTests/Shader.nzslb");
+		CheckHeaderMatch("UnitTests/Shader.spv");
 	}
 }
