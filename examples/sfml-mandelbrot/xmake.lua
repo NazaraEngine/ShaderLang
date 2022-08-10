bcab8b8880b21e7b2f5e1e9e759f2a18d127d274
Jérôme Leclercq <lynix680@gmail.com>
2022-08-10 14:15:56 +0200

    sfml-mandelbrot: Install resources

--- examples/sfml-mandelbrot/xmake.lua
+++ examples/sfml-mandelbrot/xmake.lua
@@ -2,8 +2,11 @@ add_requires("sfml", { configs = { audio = false, network = false }})
 
 target("sfml-mandelbrot")
 	set_group("Examples")
-	add_files("main.cpp")
-	add_headerfiles("mandelbrot.nzsl", { install = false })
 	add_packages("sfml")
 	add_deps("nzsl")
 	set_rundir(".")
+
+	add_files("main.cpp")
+	add_headerfiles("mandelbrot.nzsl", { install = false })
+	add_installfiles("mandelbrot.nzsl", {prefixdir = "bin"})
+	add_installfiles("palette.png", {prefixdir = "bin"})
