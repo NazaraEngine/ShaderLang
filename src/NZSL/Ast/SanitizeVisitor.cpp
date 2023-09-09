NazaraEngine/ShaderLang

commit c7207defa7f1f3916d84d338618c194db987d1ea

Compiler: Split assignation of wrapped structs/arrays
This is required when generating SPIR-V because the uniform/storage layout of structs and arrays is not compatible with the variable storage of the same type (different ArrayStride)

src/NZSL/Ast/SanitizeVisitor.cpp
@@ -410,19 +410,6 @@ namespace nzsl::Ast
 				const StructDescription::StructMember* fieldPtr = nullptr;
 				for (const auto& field : s->members)
 				{
-					if (field.cond.HasValue())
-					{
-						if (!field.cond.IsResultingValue())
-						{
-							if (m_context->options.partialSanitization)
-								return Cloner::Clone(node); //< unresolved
-
-							throw CompilerConstantExpressionRequiredError{ field.cond.GetExpression()->sourceLocation };
-						}
-						else if (!field.cond.GetResultingValue())
-							continue;
-					}
-
 					if (!field.originalName.empty())
 					{
 						if (field.originalName == identifierEntry.identifier)
@@ -446,6 +433,19 @@ namespace nzsl::Ast
 						return Cloner::Clone(node); //< unresolved
 
 					throw CompilerUnknownFieldError{ indexedExpr->sourceLocation, identifierEntry.identifier };
+				}
+
+				if (fieldPtr->cond.HasValue())
+				{
+					if (!fieldPtr->cond.IsResultingValue())
+					{
+						if (m_context->options.partialSanitization)
+							return Cloner::Clone(node); //< unresolved
+
+						throw CompilerConstantExpressionRequiredError{ fieldPtr->cond.GetExpression()->sourceLocation };
+					}
+					else if (!fieldPtr->cond.GetResultingValue())
+						continue;
 				}
 
 				if (fieldPtr->builtin.HasValue() && fieldPtr->builtin.IsResultingValue())
@@ -601,7 +601,87 @@ namespace nzsl::Ast
 		MandatoryExpr(node.right, node.sourceLocation);
 
 		auto clone = Nz::StaticUniquePointerCast<AssignExpression>(Cloner::Clone(node));
-		Validate(*clone);
+		if (Validate(*clone) == ValidationResult::Unresolved)
+			return clone;
+
+		if (m_context->options.splitWrappedStructAssignation || m_context->options.splitWrappedArrayAssignation)
+		{
+			const ExpressionType& targetType = ResolveAlias(GetExpressionTypeSecure(*clone->left));
+			const ExpressionType& sourceType = ResolveAlias(GetExpressionTypeSecure(*clone->right));
+			if (m_context->options.splitWrappedStructAssignation && IsStructType(targetType) && targetType != sourceType)
+			{
+				std::size_t structIndex = std::get<StructType>(targetType).structIndex;
+				const StructDescription* desc = m_context->structs.Retrieve(structIndex, clone->sourceLocation);
+
+				auto dstVar = CacheResult(std::move(clone->left));
+				auto srcVar = CacheResult(std::move(clone->right));
+
+				std::int32_t memberIndex = 0;
+				for (auto& member : desc->members)
+				{
+					if (member.cond.IsResultingValue() && !member.cond.GetResultingValue())
+					{
+						memberIndex++;
+						continue;
+					}
+
+					ExpressionPtr dstAccess;
+					ExpressionPtr srcAccess;
+					if (m_context->options.useIdentifierAccessesForStructs)
+					{
+						dstAccess = ShaderBuilder::AccessMember(CloneExpression(*dstVar), { member.name });
+						srcAccess = ShaderBuilder::AccessMember(CloneExpression(*srcVar), { member.name });
+					}
+					else
+					{
+						dstAccess = ShaderBuilder::AccessIndex(CloneExpression(*dstVar), memberIndex);
+						srcAccess = ShaderBuilder::AccessIndex(CloneExpression(*srcVar), memberIndex);
+					}
+
+					dstAccess->sourceLocation = clone->sourceLocation;
+					srcAccess->sourceLocation = clone->sourceLocation;
+
+					auto assign = ShaderBuilder::Assign(AssignType::Simple, std::move(dstAccess), std::move(srcAccess));
+					assign->sourceLocation = clone->sourceLocation;
+
+					StatementPtr assignStatement = ShaderBuilder::ExpressionStatement(std::move(assign));
+
+					if (member.cond.IsExpression())
+						assignStatement = ShaderBuilder::ConstBranch(CloneExpression(*member.cond.GetExpression()), std::move(assignStatement));
+					
+					m_context->currentStatementList->push_back(CloneStatement(*assignStatement));
+
+					memberIndex++;
+				}
+
+				return dstVar;
+			}
+			else if (m_context->options.splitWrappedArrayAssignation && IsArrayType(targetType) && targetType != sourceType)
+			{
+				const auto& arrayType = std::get<ArrayType>(targetType);
+
+				auto dstVar = CacheResult(std::move(clone->left));
+				auto srcVar = CacheResult(std::move(clone->right));
+
+				for (std::uint32_t i = 0; i < arrayType.length; ++i)
+				{
+					ExpressionPtr dstAccess = ShaderBuilder::AccessIndex(CloneExpression(*dstVar), Nz::SafeCast<std::int32_t>(i));
+					ExpressionPtr srcAccess = ShaderBuilder::AccessIndex(CloneExpression(*srcVar), Nz::SafeCast<std::int32_t>(i));
+
+					dstAccess->sourceLocation = clone->sourceLocation;
+					srcAccess->sourceLocation = clone->sourceLocation;
+
+					auto assign = ShaderBuilder::Assign(AssignType::Simple, std::move(dstAccess), std::move(srcAccess));
+					assign->sourceLocation = clone->sourceLocation;
+
+					StatementPtr assignStatement = ShaderBuilder::ExpressionStatement(std::move(assign));
+
+					m_context->currentStatementList->push_back(CloneStatement(*assignStatement));
+				}
+
+				return dstVar;
+			}
+		}
 
 		return clone;
 	}
@@ -1796,7 +1876,47 @@ namespace nzsl::Ast
 			throw CompilerVarDeclarationOutsideOfFunctionError{ node.sourceLocation };
 
 		auto clone = Nz::StaticUniquePointerCast<DeclareVariableStatement>(Cloner::Clone(node));
-		Validate(*clone);
+		if (Validate(*clone) == ValidationResult::Unresolved)
+			return clone;
+
+		if (clone->initialExpression)
+		{
+			const ExpressionType& resolvedType = ResolveAlias(clone->varType.GetResultingValue());
+
+			auto ShouldSplit = [&]
+			{
+				if (m_context->options.splitWrappedStructAssignation && IsStructType(resolvedType))
+				{
+					return resolvedType != ResolveAlias(GetExpressionTypeSecure(*clone->initialExpression));
+				}
+				else if (m_context->options.splitWrappedArrayAssignation && IsArrayType(resolvedType))
+				{
+					return resolvedType != ResolveAlias(GetExpressionTypeSecure(*clone->initialExpression));
+				}
+
+				return false;
+			};
+
+			if (ShouldSplit())
+			{
+				// Split variable declaration and assignation
+				ExpressionPtr assign = ShaderBuilder::Assign(AssignType::Simple, ShaderBuilder::Variable(*clone->varIndex, clone->varType.GetResultingValue(), clone->sourceLocation), std::move(clone->initialExpression));
+
+				MultiStatementPtr multiStatement = ShaderBuilder::MultiStatement();
+				multiStatement->sourceLocation = clone->sourceLocation;
+				multiStatement->statements.push_back(std::move(clone));
+
+				std::vector<StatementPtr>* previousList = m_context->currentStatementList;
+				m_context->currentStatementList = &multiStatement->statements;
+
+				// Clone assign to replace it with a per-field assignation
+				multiStatement->statements.push_back(CloneStatement(ShaderBuilder::ExpressionStatement(std::move(assign))));
+
+				m_context->currentStatementList = previousList;
+
+				return multiStatement;
+			}
+		}
 
 		return clone;
 	}
@@ -1815,7 +1935,11 @@ namespace nzsl::Ast
 	{
 		MandatoryExpr(node.expression, node.sourceLocation);
 
-		return Cloner::Clone(node);
+		auto exprStatement = Nz::StaticUniquePointerCast<ExpressionStatement>(Cloner::Clone(node));
+		if (GetExpressionCategory(*exprStatement->expression) == ExpressionCategory::LValue)
+			return ShaderBuilder::NoOp();
+
+		return exprStatement;
 	}
 
 	StatementPtr SanitizeVisitor::Clone(ForStatement& node)
@@ -4150,9 +4274,7 @@ namespace nzsl::Ast
 		switch (node.op)
 		{
 			case AssignType::Simple:
-				if (TypeMustMatch(node.left, node.right, node.sourceLocation) == ValidationResult::Unresolved)
-					return ValidationResult::Unresolved;
-
+				TypeMustMatch(*leftExprType, UnwrapExternalType(ResolveAlias(*rightExprType)), node.sourceLocation);
 				break;
 
 			case AssignType::CompoundAdd:        binaryType = BinaryType::Add; break;
@@ -4167,7 +4289,7 @@ namespace nzsl::Ast
 		if (binaryType)
 		{
 			ExpressionType expressionType = ValidateBinaryOp(*binaryType, ResolveAlias(*leftExprType), UnwrapExternalType(ResolveAlias(*rightExprType)), node.sourceLocation);
-			TypeMustMatch(*leftExprType, expressionType, node.sourceLocation);
+			TypeMustMatch(UnwrapExternalType(*leftExprType), expressionType, node.sourceLocation);
 
 			if (m_context->options.removeCompoundAssignments)
 			{
@@ -5770,6 +5892,17 @@ namespace nzsl::Ast
 			return std::get<StorageType>(exprType).containedType;
 		else if (IsUniformType(exprType))
 			return std::get<UniformType>(exprType).containedType;
+		else if (IsArrayType(exprType))
+		{
+			const ArrayType& arrayType = std::get<ArrayType>(exprType);
+
+			ArrayType wrappedArrayType;
+			wrappedArrayType.containedType = std::make_unique<ContainedType>();
+			wrappedArrayType.containedType->type = UnwrapExternalType(arrayType.containedType->type);
+			wrappedArrayType.length = arrayType.length;
+
+			return wrappedArrayType;
+		}
 		else
 			return exprType;
 	}
@@ -5785,12 +5918,12 @@ namespace nzsl::Ast
 		else if (IsArrayType(exprType))
 		{
 			const ArrayType& arrayType = std::get<ArrayType>(exprType);
-			ExpressionType wrapperInnerType = WrapExternalType<T>(arrayType.containedType->type);
 
 			ArrayType wrappedArrayType;
 			wrappedArrayType.containedType = std::make_unique<ContainedType>();
-			wrappedArrayType.containedType->type = wrapperInnerType;
+			wrappedArrayType.containedType->type = WrapExternalType<T>(arrayType.containedType->type);
 			wrappedArrayType.length = arrayType.length;
+			wrappedArrayType.isWrapped = true;
 
 			return wrappedArrayType;
 		}

include/NZSL/Ast/ExpressionType.hpp
@@ -42,6 +42,7 @@ namespace nzsl::Ast
 		BaseArrayType& operator=(BaseArrayType&&) noexcept = default;
 
 		std::unique_ptr<ContainedType> containedType;
+		bool isWrapped = false;
 
 		bool operator==(const BaseArrayType& rhs) const;
 		inline bool operator!=(const BaseArrayType& rhs) const;

include/NZSL/Ast/SanitizeVisitor.hpp
@@ -56,6 +56,8 @@ namespace nzsl::Ast
 				bool removeScalarSwizzling = false;
 				bool removeSingleConstDeclaration = false;
 				bool splitMultipleBranches = false;
+				bool splitWrappedArrayAssignation = false;
+				bool splitWrappedStructAssignation = false;
 				bool useIdentifierAccessesForStructs = true;
 			};
 

include/NZSL/SpirvWriter.hpp
@@ -11,6 +11,7 @@
 #include <NZSL/ShaderWriter.hpp>
 #include <NZSL/Ast/ConstantValue.hpp>
 #include <NZSL/Ast/Module.hpp>
+#include <NZSL/Ast/SanitizeVisitor.hpp>
 #include <NZSL/SpirV/SpirvConstantCache.hpp>
 #include <NZSL/SpirV/SpirvVariable.hpp>
 #include <string>
@@ -51,6 +52,7 @@ namespace nzsl
 			};
 			
 			static std::pair<std::uint32_t, std::uint32_t> GetMaximumSupportedVersion(std::uint32_t vkMajorVersion, std::uint32_t vkMinorVersion);
+			static Ast::SanitizeVisitor::Options GetSanitizeOptions();
 
 		private:
 			struct FunctionParameter;

src/NZSL/Ast/AstSerializer.cpp
@@ -12,7 +12,7 @@ namespace nzsl::Ast
 	namespace
 	{
 		constexpr std::uint32_t s_shaderAstMagicNumber = 0x4E534852;
-		constexpr std::uint32_t s_shaderAstCurrentVersion = 7;
+		constexpr std::uint32_t s_shaderAstCurrentVersion = 8;
 
 		class ShaderSerializerVisitor : public ExpressionVisitor, public StatementVisitor
 		{
@@ -634,6 +634,8 @@ namespace nzsl::Ast
 				m_serializer.Serialize(std::uint8_t(8));
 				Value(arg.length);
 				Type(arg.containedType->type);
+				if (IsVersionGreaterOrEqual(8))
+					Value(arg.isWrapped);
 			}
 			else if constexpr (std::is_same_v<T, Ast::Type>)
 			{
@@ -1080,6 +1082,9 @@ namespace nzsl::Ast
 				arrayType.length = length;
 				arrayType.containedType = std::make_unique<ContainedType>();
 				arrayType.containedType->type = std::move(containedType);
+
+				if (IsVersionGreaterOrEqual(8))
+					Value(arrayType.isWrapped);
 
 				type = std::move(arrayType);
 				break;

src/NZSL/Ast/ExpressionType.cpp
@@ -64,6 +64,9 @@ namespace nzsl::Ast
 		assert(containedType);
 		assert(rhs.containedType);
 
+		if (isWrapped != rhs.isWrapped)
+			return false;
+
 		if (containedType->type != rhs.containedType->type)
 			return false;
 

src/NZSL/SpirvWriter.cpp
@@ -617,19 +617,9 @@ namespace nzsl
 		const Ast::Module* targetModule;
 		if (!states.sanitized)
 		{
-			Ast::SanitizeVisitor::Options options;
-			options.moduleResolver = states.shaderModuleResolver;
+			Ast::SanitizeVisitor::Options options = GetSanitizeOptions();
 			options.optionValues = states.optionValues;
-			options.reduceLoopsToWhile = true;
-			options.removeAliases = true;
-			options.removeCompoundAssignments = true;
-			options.removeConstArraySize = true;
-			options.removeMatrixBinaryAddSub = true;
-			options.removeMatrixCast = true;
-			options.removeOptionDeclaration = true;
-			options.removeSingleConstDeclaration = true;
-			options.splitMultipleBranches = true;
-			options.useIdentifierAccessesForStructs = false;
+			options.moduleResolver = states.shaderModuleResolver;
 
 			sanitizedModule = Ast::Sanitize(module, options);
 			targetModule = sanitizedModule.get();
@@ -836,6 +826,25 @@ namespace nzsl
 			return { 1, 3 };
 		else
 			return { 1, 0 };
+	}
+
+	Ast::SanitizeVisitor::Options SpirvWriter::GetSanitizeOptions()
+	{
+		Ast::SanitizeVisitor::Options options;
+		options.reduceLoopsToWhile = true;
+		options.removeAliases = true;
+		options.removeCompoundAssignments = true;
+		options.removeConstArraySize = true;
+		options.removeMatrixBinaryAddSub = true;
+		options.removeMatrixCast = true;
+		options.removeOptionDeclaration = true;
+		options.removeSingleConstDeclaration = true;
+		options.splitWrappedArrayAssignation = true;
+		options.splitMultipleBranches = true;
+		options.splitWrappedStructAssignation = true;
+		options.useIdentifierAccessesForStructs = false;
+
+		return options;
 	}
 
 	std::uint32_t SpirvWriter::AllocateResultId()

tests/src/Tests/ExternalTests.cpp
@@ -842,7 +842,7 @@ module;
 
 struct Data
 {
-	index : i32
+	index: i32
 }
 
 external
@@ -890,10 +890,7 @@ fn main()
 
 })");
 
-		WHEN("Generating SPIR-V 1.0")
-		{
-			nzsl::SpirvWriter::Environment spirvEnv;
-			ExpectSPIRV(*shaderModule, R"(
+	ExpectSPIRV(*shaderModule, R"(
  %1 = OpTypeInt 32 1
  %2 = OpTypeStruct %1
  %3 = OpTypeStruct %1
@@ -904,7 +901,249 @@ fn main()
  %8 = OpFunction %6 FunctionControl(0) %7
  %9 = OpLabel
       OpReturn
-      OpFunctionEnd)", {}, spirvEnv, true);
+      OpFunctionEnd)", {}, {}, true);
+	}
+
+
+	SECTION("Incompatible structs test")
+	{
+		std::string_view nzslSource = R"(
+[nzsl_version("1.0")]
+module;
+
+const MaxLightCascadeCount = 4;
+const MaxLightCount = 3;
+
+[layout(std140)]
+struct DirectionalLight
+{
+	color: vec3[f32],
+	direction: vec3[f32],
+	invShadowMapSize: vec2[f32],	
+	ambientFactor: f32,
+	diffuseFactor: f32,
+	cascadeCount: u32,
+	cascadeDistances: array[f32, MaxLightCascadeCount],
+	viewProjMatrices: array[mat4[f32], MaxLightCascadeCount],
+}
+
+[layout(std140)]
+struct LightData
+{
+	directionalLights: array[DirectionalLight, MaxLightCount],
+	directionalLightCount: u32
+}
+
+external
+{
+	[binding(0)] lightData: uniform[LightData]
+}
+
+[entry(frag)]
+fn main()
+{
+	for lightIndex in u32(0) -> lightData.directionalLightCount
+	{
+		// light struct is not compatible with DirectionalLight (because of the ArrayStride), this forces a per-member copy in SPIR-V
+		let light = lightData.directionalLights[lightIndex];
+
+		// struct are compatibles, a direct copy is performed
+		let lightCopy = light;
+	}
+}
+)";
+
+		nzsl::Ast::ModulePtr shaderModule = nzsl::Parse(nzslSource);
+
+		shaderModule = SanitizeModule(*shaderModule);
+
+		ExpectGLSL(*shaderModule, R"(
+struct DirectionalLight
+{
+	vec3 color;
+	vec3 direction;
+	vec2 invShadowMapSize;
+	float ambientFactor;
+	float diffuseFactor;
+	uint cascadeCount;
+	float cascadeDistances[4];
+	mat4 viewProjMatrices[4];
+};
+
+// struct LightData omitted (used as UBO/SSBO)
+
+layout(std140) uniform _nzslBindinglightData
+{
+	DirectionalLight directionalLights[3];
+	uint directionalLightCount;
+} lightData;
+
+void main()
+{
+	{
+		uint lightIndex = uint(0);
+		uint to = lightData.directionalLightCount;
+		while (lightIndex < to)
+		{
+			DirectionalLight light = lightData.directionalLights[lightIndex];
+			DirectionalLight lightCopy = light;
+			lightIndex += 1u;
 		}
+
+	}
+
+}
+)");
+
+		ExpectNZSL(*shaderModule, R"(
+const MaxLightCascadeCount: i32 = 4;
+
+const MaxLightCount: i32 = 3;
+
+[layout(std140)]
+struct DirectionalLight
+{
+	color: vec3[f32],
+	direction: vec3[f32],
+	invShadowMapSize: vec2[f32],
+	ambientFactor: f32,
+	diffuseFactor: f32,
+	cascadeCount: u32,
+	cascadeDistances: array[f32, 4],
+	viewProjMatrices: array[mat4[f32], 4]
+}
+
+[layout(std140)]
+struct LightData
+{
+	directionalLights: array[DirectionalLight, 3],
+	directionalLightCount: u32
+}
+
+external
+{
+	[set(0), binding(0)] lightData: uniform[LightData]
+}
+
+[entry(frag)]
+fn main()
+{
+	for lightIndex in u32(0) -> lightData.directionalLightCount
+	{
+		let light: DirectionalLight = lightData.directionalLights[lightIndex];
+		let lightCopy: DirectionalLight = light;
+	}
+
+})");
+
+		ExpectSPIRV(*shaderModule, R"(
+ %36 = OpFunction %21 FunctionControl(0) %22
+ %37 = OpLabel
+ %38 = OpVariable %25 StorageClass(Function)
+ %39 = OpVariable %25 StorageClass(Function)
+ %40 = OpVariable %28 StorageClass(Function)
+ %41 = OpVariable %28 StorageClass(Function)
+ %42 = OpBitcast %4 %24
+       OpStore %38 %42
+ %44 = OpAccessChain %43 %20 %26
+ %45 = OpLoad %4 %44
+       OpStore %39 %45
+       OpBranch %46
+ %46 = OpLabel
+ %50 = OpLoad %4 %38
+ %51 = OpLoad %4 %39
+ %52 = OpULessThan %27 %50 %51
+       OpLoopMerge %48 %49 LoopControl(0)
+       OpBranchConditional %52 %47 %48
+ %47 = OpLabel
+ %53 = OpLoad %4 %38
+ %55 = OpAccessChain %54 %20 %24 %53 %24
+ %56 = OpLoad %2 %55
+ %57 = OpAccessChain %58 %40 %24
+       OpStore %57 %56
+ %59 = OpLoad %4 %38
+ %60 = OpAccessChain %54 %20 %24 %59 %26
+ %61 = OpLoad %2 %60
+ %62 = OpAccessChain %58 %40 %26
+       OpStore %62 %61
+ %63 = OpLoad %4 %38
+ %65 = OpAccessChain %64 %20 %24 %63 %29
+ %66 = OpLoad %3 %65
+ %67 = OpAccessChain %68 %40 %29
+       OpStore %67 %66
+ %69 = OpLoad %4 %38
+ %71 = OpAccessChain %70 %20 %24 %69 %30
+ %72 = OpLoad %1 %71
+ %73 = OpAccessChain %74 %40 %30
+       OpStore %73 %72
+ %75 = OpLoad %4 %38
+ %76 = OpAccessChain %70 %20 %24 %75 %31
+ %77 = OpLoad %1 %76
+ %78 = OpAccessChain %74 %40 %31
+       OpStore %78 %77
+ %79 = OpLoad %4 %38
+ %80 = OpAccessChain %43 %20 %24 %79 %32
+ %81 = OpLoad %4 %80
+ %82 = OpAccessChain %25 %40 %32
+       OpStore %82 %81
+ %83 = OpLoad %4 %38
+ %84 = OpAccessChain %70 %20 %24 %83 %33 %24
+ %85 = OpLoad %1 %84
+ %86 = OpAccessChain %87 %40 %33
+ %88 = OpAccessChain %74 %86 %24
+       OpStore %88 %85
+ %89 = OpLoad %4 %38
+ %90 = OpAccessChain %70 %20 %24 %89 %33 %26
+ %91 = OpLoad %1 %90
+ %92 = OpAccessChain %87 %40 %33
+ %93 = OpAccessChain %74 %92 %26
+       OpStore %93 %91
+ %94 = OpLoad %4 %38
+ %95 = OpAccessChain %70 %20 %24 %94 %33 %29
+ %96 = OpLoad %1 %95
+ %97 = OpAccessChain %87 %40 %33
+ %98 = OpAccessChain %74 %97 %29
+       OpStore %98 %96
+ %99 = OpLoad %4 %38
+%100 = OpAccessChain %70 %20 %24 %99 %33 %30
+%101 = OpLoad %1 %100
+%102 = OpAccessChain %87 %40 %33
+%103 = OpAccessChain %74 %102 %30
+       OpStore %103 %101
+%104 = OpLoad %4 %38
+%106 = OpAccessChain %105 %20 %24 %104 %34 %24
+%107 = OpLoad %8 %106
+%108 = OpAccessChain %109 %40 %34
+%110 = OpAccessChain %111 %108 %24
+       OpStore %110 %107
+%112 = OpLoad %4 %38
+%113 = OpAccessChain %105 %20 %24 %112 %34 %26
+%114 = OpLoad %8 %113
+%115 = OpAccessChain %109 %40 %34
+%116 = OpAccessChain %111 %115 %26
+       OpStore %116 %114
+%117 = OpLoad %4 %38
+%118 = OpAccessChain %105 %20 %24 %117 %34 %29
+%119 = OpLoad %8 %118
+%120 = OpAccessChain %109 %40 %34
+%121 = OpAccessChain %111 %120 %29
+       OpStore %121 %119
+%122 = OpLoad %4 %38
+%123 = OpAccessChain %105 %20 %24 %122 %34 %30
+%124 = OpLoad %8 %123
+%125 = OpAccessChain %109 %40 %34
+%126 = OpAccessChain %111 %125 %30
+       OpStore %126 %124
+%127 = OpLoad %10 %40
+       OpStore %41 %127
+%128 = OpLoad %4 %38
+%129 = OpIAdd %4 %128 %35
+       OpStore %38 %129
+       OpBranch %49
+ %49 = OpLabel
+       OpBranch %46
+ %48 = OpLabel
+       OpReturn
+       OpFunctionEnd)", {}, {}, true);
 	}
 }

commit 2cbba61ddcf5cc2122796a401479b2246120aa86

Compiler: Fix AccessIdentifierExpression => AccessIndexExpression erronous field indices when preceding fields have unresolved cond
Fix a bug introduced in c7207defa7f1f3916d84d338618c194db987d1ea

src/NZSL/Ast/SanitizeVisitor.cpp
@@ -408,8 +408,12 @@ namespace nzsl::Ast
 				// Retrieve member index (not counting disabled fields)
 				std::int32_t fieldIndex = 0;
 				const StructDescription::StructMember* fieldPtr = nullptr;
+				bool hasUnresolvedFields = false;
 				for (const auto& field : s->members)
 				{
+					if (field.cond.HasValue() && !field.cond.IsResultingValue())
+						hasUnresolvedFields = true;
+
 					if (!field.originalName.empty())
 					{
 						if (field.originalName == identifierEntry.identifier)
@@ -426,6 +430,9 @@ namespace nzsl::Ast
 
 					fieldIndex++;
 				}
+
+				if (hasUnresolvedFields)
+					fieldIndex = -1; //< field index is not unknown because some fields before it are not resolved
 
 				if (!fieldPtr)
 				{
@@ -493,6 +500,9 @@ namespace nzsl::Ast
 				}
 				else
 				{
+					if (fieldIndex < 0)
+						return Cloner::Clone(node); //< unresolved
+
 					// Transform to AccessIndexExpression
 					std::unique_ptr<AccessIndexExpression> accessIndex = std::make_unique<AccessIndexExpression>();
 					accessIndex->sourceLocation = indexedExpr->sourceLocation;