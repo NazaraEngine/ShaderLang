b10e97df6ae972a6711c1c63394edf10f9e809f8
SirLynix <lynix680@gmail.com>
2024-07-03 21:09:05 +0200

    Compiler: Continue multipass implementation (WIP)

---
+++ src/NZSL/Ast/Transformations/AssignmentTransformer.cpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2024 Jérôme "SirLynix" Leclercq (lynix680@gmail.com)
+// This file is part of the "Nazara Shading Language" project
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <NZSL/Ast/Transformations/AssignmentTransformer.hpp>
+#include <NZSL/Ast/Cloner.hpp>
+#include <NZSL/Lang/Errors.hpp>
+
+namespace nzsl::Ast
+{
+	bool AssignmentTransformer::Transform(Module& module, Context& context, const Options& options, std::string* error)
+	{
+		m_options = &options;
+
+		return TransformModule(module, context, error);
+	}
+
+	ExpressionPtr AssignmentTransformer::Transform(AssignExpression&& assign)
+	{
+		if (assign.op == AssignType::Simple || !m_options->removeCompoundAssignment)
+			return nullptr;
+
+		BinaryType binaryType;
+		switch (assign.op)
+		{
+			case AssignType::Simple:             NAZARA_UNREACHABLE();
+			case AssignType::CompoundAdd:        binaryType = BinaryType::Add; break;
+			case AssignType::CompoundDivide:     binaryType = BinaryType::Divide; break;
+			case AssignType::CompoundModulo:     binaryType = BinaryType::Modulo; break;
+			case AssignType::CompoundMultiply:   binaryType = BinaryType::Multiply; break;
+			case AssignType::CompoundLogicalAnd: binaryType = BinaryType::LogicalAnd; break;
+			case AssignType::CompoundLogicalOr:  binaryType = BinaryType::LogicalOr; break;
+			case AssignType::CompoundSubtract:   binaryType = BinaryType::Subtract; break;
+		}
+
+		assign.op = AssignType::Simple;
+		assign.right = ShaderBuilder::Binary(binaryType, Clone(*assign.left), std::move(assign.right));
+
+		return nullptr;
+	}
+}
