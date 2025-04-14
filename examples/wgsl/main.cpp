83e683236ea7ec869e58866222e52c1f300d3b3f
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-04-14 21:42:47 +0200

    working on external statements ci skip

--- examples/wgsl/main.cpp
+++ examples/wgsl/main.cpp
@@ -5,26 +5,8 @@
 int main()
 {
 	auto shader = nzsl::ParseFromFile("shader.nzsl");
-
 	nzsl::WgslWriter wgslWriter;
 	auto wgslShader = wgslWriter.Generate(*shader);
-
 	std::cout << wgslShader << std::endl;
-
-	std::cout << "\n=============================================================\n" << R"(
-struct Outputs
-{
-    @location(0) color: vec4<f32>;
-}
-
-@fragment
-fn main() -> Outputs
-{
-    var output: Outputs;
-    output.color = vec4<f32>(1.0, 1.0, 1.0, 1.0);
-    return output;
-}
-	)" << std::endl;
-
 	return 0;
 }
