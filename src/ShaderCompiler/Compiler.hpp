a64b457feac261e5329ed5653c14f132a4dc63ce
SirLynix <lynix680@gmail.com>
2025-07-17 08:25:00 +0200

    Compiler: Validate module after resolving step

--- src/ShaderCompiler/Compiler.hpp
+++ src/ShaderCompiler/Compiler.hpp
@@ -58,7 +58,7 @@ namespace nzslc
 			void OutputFile(std::filesystem::path filePath, const void* data, std::size_t size, bool disallowHeader = false);
 			void OutputToStdout(std::string_view str);
 			void ReadInput();
-			void Sanitize();
+			void Resolve();
 			template<typename F, typename... Args> auto Step(std::enable_if_t<!std::is_member_function_pointer_v<F>, std::string_view> stepName, F&& func, Args&&... args) -> decltype(std::invoke(func, std::forward<Args>(args)...));
 			template<typename F, typename... Args> auto Step(std::enable_if_t<std::is_member_function_pointer_v<F>, std::string_view> stepName, F&& func, Args&&... args) -> decltype(std::invoke(func, this, std::forward<Args>(args)...));
 			template<typename F> auto StepInternal(std::string_view stepName, F&& func) -> decltype(func());
