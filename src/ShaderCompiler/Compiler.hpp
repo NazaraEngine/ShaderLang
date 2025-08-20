e9b08ea7b02b9a8205b519d363823256c28f80b3
SirLynix <lynix680@gmail.com>
2025-08-20 10:54:11 +0200

    Update nzsla and nzslc tool version to 1.0.0

--- src/ShaderCompiler/Compiler.hpp
+++ src/ShaderCompiler/Compiler.hpp
@@ -25,8 +25,8 @@ namespace nzslc
 	class Compiler
 	{
 		public:
-			static constexpr std::uint32_t MajorVersion = 0;
-			static constexpr std::uint32_t MinorVersion = 1;
+			static constexpr std::uint32_t MajorVersion = 1;
+			static constexpr std::uint32_t MinorVersion = 0;
 			static constexpr std::uint32_t PatchVersion = 0;
 
 			Compiler(cxxopts::ParseResult& options);
