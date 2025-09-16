bc91adbe092e8c64797a1629797ca5d2dcb84686
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-09-16 08:39:00 +0200

    removing test example

--- examples/wgsl/xmake.lua
+++
@@ -1,8 +0,0 @@
-target("wgsl-example")
-	set_group("Examples")
-	add_deps("nzsl")
-	set_rundir(".")
-
-	add_files("main.cpp")
-	add_headerfiles("shader.nzsl", { install = false })
-	add_installfiles("shader.nzsl", {prefixdir = "bin"})
