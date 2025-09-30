e4a696745ec26d00fbb1583575039d7ec00fc3a9
SirLynix <lynix680@gmail.com>
2025-09-30 18:26:08 +0200

    Prepare for 1.1 release

--- include/NZSL/Config.hpp
+++ include/NZSL/Config.hpp
@@ -33,7 +33,7 @@
 #define NZSL_VERSION_MAJOR 1
 #define NZSL_VERSION_MINOR 1
 #define NZSL_VERSION_PATCH 0
-#define NZSL_VERSION_SUFFIX "-rc3"
+#define NZSL_VERSION_SUFFIX ""
 
 #if !defined(NZSL_STATIC)
 	#ifdef NZSL_BUILD
